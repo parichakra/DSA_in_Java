ace9a6911ce5560bbb102e692a01d2caf11fdf34
parichakra <khatrivikram005@gmail.com>
2025-04-10 23:37:39 +0545

    controlllflow

---
+++ Java_Basics/02_controlflow/doWhile/doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile{
+    public static void main ( String [] args){
+        int i = 1;
+        do{ 
+            System.out.println( " count: "+i);
+            i++;
+        }while( i<= 5);
+    }
+}
