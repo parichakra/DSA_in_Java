d4be26aea5ee89056132b4878105644d9eb31752
parichakra <khatrivikram005@gmail.com>
2025-04-17 22:52:26 +0545

    modified

--- 01_Java_Basics/05_String_stringBuilder/stringBuilders.java
+++ 01_Java_Basics/05_String_stringBuilder/stringBuilders.java
@@ -1,6 +1,6 @@
 public class stringBuilders {
     public static void main ( String [] args){
-        String str1 = "Hello "; //
+        // String str1 = "Hello "; 
 
         StringBuilder sb = new StringBuilder(" Hello");
 
