4f99820b1e6d2becb7bed6a6fb9123065dc73867
parichakra <khatrivikram005@gmail.com>
2025-04-17 10:53:58 +0545

    basics array

---
+++ Java_Basics/04_Arrays/arrayBasics.java
@@ -0,0 +1,19 @@
+public class arrayBasics{
+    public static void main ( String [] args){
+        int[] numbers;
+
+        numbers = new int[5];
+
+        int[] numbers1 = { 10,20,30,40,50};
+
+        System.out.println(numbers1[2]);
+        numbers1[2]= 22;
+        System.out.println(numbers1[2]);
+
+
+        for (int num : numbers) {
+                System.out.println(numbers1);
+            }
+
+    }
+}
