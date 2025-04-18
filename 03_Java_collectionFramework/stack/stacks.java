6eeba81c5d3c8180baa5a9ff89177fbd45b1d48c
parichakra <khatrivikram005@gmail.com>
2025-04-18 16:26:22 +0545

    link list updatae

---
+++ 03_Java_collectionFramework/stack/stacks.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+
+public class stacks{
+    public static void main ( String [] args){
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.peek());
+
+
+
+
+    }
+}
