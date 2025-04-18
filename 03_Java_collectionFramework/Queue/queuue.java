c3447252ddfc095e8929c4e3ea22dd2518be0c38
parichakra <khatrivikram005@gmail.com>
2025-04-18 16:24:53 +0545

    queue

---
+++ 03_Java_collectionFramework/Queue/queuue.java
@@ -0,0 +1,20 @@
+
+
+public class queuue{
+  
+
+   public static void main(String[] var0) {
+      // Queue que= new Queue();
+
+      // Stack var1 = new Stack();
+      // var1.push(10);
+      // var1.push(20);
+      // var1.push(30);
+      // var1.push(40);
+      // System.out.println(var1);
+      // System.out.println(var1.peek());
+      // var1.pop();
+      // System.out.println(var1);
+      // System.out.println(var1.peek());
+   }
+}
