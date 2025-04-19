36ea780a9519600b8e23d4c4f46cf2103bd880df
parichakra <khatrivikram005@gmail.com>
2025-04-19 23:35:50 +0545

    hashMap

---
+++ 03_Java_collectionFramework/map/hashMap.java
@@ -0,0 +1,37 @@
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+
+public class hashMap {
+
+
+   public static void main(String[] var0) {
+
+      //hashamp
+   HashMap<String, Integer> hmap= new HashMap<String,Integer>();
+
+   hmap.put("second", 20);
+
+   hmap.put("first", 30);
+
+   
+
+
+    System.out.println("Hashmapque="+hmap);
+
+    TreeMap<String, Integer> Tmap= new TreeMap<String, Integer>();
+
+   Tmap.put("second", 20);
+ 
+   Tmap.put("first", 30);
+ 
+    
+ 
+ 
+     System.out.println("Hashmapque="+Tmap);
+
+      
+   }
+}
+  
